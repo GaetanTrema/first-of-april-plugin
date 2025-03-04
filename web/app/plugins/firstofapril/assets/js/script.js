c21bca177457f929757a9a53a8fed536b75c7fd3
GaetanTrema <gaetantrema@gmail.com>
2025-03-04 11:31:24 +0100

    :recycle: Tween declaration

--- web/app/plugins/firstofapril/assets/js/script.js
+++ web/app/plugins/firstofapril/assets/js/script.js
@@ -3,47 +3,46 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('a');
     links.forEach(link => {
         link.addEventListener('mouseenter', function(e) {
-            gsap.to("#jamy",
-            {
-                opacity: 1, 
-                y: e.clientY, 
-                x: e.clientX, 
-                duration: 0.5, 
-                overwrite: true 
-            });
+           jumpTo("#jamy", e.clientX, e.clientY);
         });
 
         link.addEventListener('mouseleave', function(e) {
-            gsap.to("#jamy", { 
-                y: window.innerHeight, 
-                x: Math.random() * window.innerWidth, 
-                delay: 0.5,
-                duration: 0.5
-            });
+            disappearRandom("#jamy");
         });
     });
 
     const formInputs = document.querySelectorAll('input, textarea');
     formInputs.forEach(input => {
         input.addEventListener('focus', function(e) {
-            gsap.to("#chuck",
-            {
-                opacity: 1, 
-                y: input.getBoundingClientRect().top, 
-                x: input.getBoundingClientRect().left, 
-                duration: 0.5, 
-                overwrite: true 
-            });
+            jumpTo(
+                "#chuck",
+                input.getBoundingClientRect().top,
+                input.getBoundingClientRect().left
+            );
         });
 
         input.addEventListener('blur', function(e) {
-            gsap.to("#chuck", { 
-                y: window.innerHeight, 
-                x: Math.random() * window.innerWidth, 
-                delay: 0.5,
-                duration: 0.5
-            });
+            disappearRandom("#chuck");
         });
     });
 
 });
+
+function jumpTo(selector, x, y) {
+    gsap.to(selector,{
+        opacity: 1, 
+        y: y, 
+        x: x, 
+        duration: 0.5, 
+        overwrite: true 
+    });
+}
+
+function disappearRandom(selector) {
+    gsap.to(selector, { 
+        y: window.innerHeight, 
+        x: Math.random() * window.innerWidth, 
+        delay: 0.5,
+        duration: 0.5
+    });
+}
