ae0dc232e3e0af54974919af110406e88ebf6870
GaetanTrema <gaetantrema@gmail.com>
2025-03-11 11:31:22 +0100

    :bug: fixes the default selection in interaction select

--- web/app/plugins/firstofapril/templates/interaction-list.php
+++ web/app/plugins/firstofapril/templates/interaction-list.php
@@ -2,8 +2,8 @@
     <select id="foa-interaction-list" name="<?= FOA\App::INTERACTION_LIST_OPTION ?>">
     <?php
     foreach ($interactionList as $interaction => $characterId) {
-        $selected = in_array($post->ID, $interactionList) ? 'selected' : '';
-        echo '<option value="'.$interaction.'" "'. $selected . '>' . $interaction .'</option>';
+        $selected = $post->ID == $characterId ? 'selected' : '';
+        echo '<option value="'.$interaction.'" '. $selected . '>' . $interaction .'</option>';
     }
     ?>
         <option value="off">Not used</option>
